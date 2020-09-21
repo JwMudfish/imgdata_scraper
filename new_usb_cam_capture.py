4337f5bcc4425a298afe4cacd72298fad3d75bad
perth <jj61183@gmail.com>
2020-09-21 18:18:36 +0900

    add box_name logic, label sorting logic

--- new_usb_cam_capture.py
+++ new_usb_cam_capture.py
@@ -13,6 +13,9 @@ import time
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 import os
+from skimage.measure import compare_ssim
+from PIL import Image, ImageFont, ImageDraw
+
 
 #####################################################################################
 #LABELS = ['galbae_can','coca_can','sprite_can']
@@ -95,9 +98,6 @@ def make_folder(label_dir):
 
 
 
-
-
-
 #####################################################################################
 
 #LABELS = get_labels('./label.txt')
@@ -141,19 +141,22 @@ while True:
     ret, frame = cap.read()
 
     box = get_boxes('./boxes')['{}'.format(BOX_NUM)]
+    box_name = sorted(os.listdir('./boxes'))
     LABELS = get_labels('./label.txt')
     #frame = cv2.resize(frame, (frame_width, frame_height))
     #frame = imutils.rotate(frame, 90) 
 
     if MODE =='b':
-        #x = 400
+        textSize = -60
         for i, j in zip(box, LABELS):
             cv2.rectangle(frame, (i[0],i[1]), (i[2], i[3]), (0,0,255), 2)
-            cv2.putText(frame, j, (i[0], i[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 4)
-            #cv2.putText(frame, '{} ||'.format(j), (x, 50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 3)
-            #x = x + 500
+            #cv2.putText(frame, j, (i[0], i[1] - 20), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,255,0), 4)
 
-        cv2.putText(frame, 'BOX : {}'.format(BOX_NUM), (0, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            cv2.putText(frame, '{} /'.format(j), (textSize + 70, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            label_len = cv2.getTextSize(text=str(j+'//'), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, thickness=2)[0][0]
+            textSize = label_len + textSize
+            
+        cv2.putText(frame, 'BOX : {}_{}'.format(BOX_NUM, box_name[int(BOX_NUM)]), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 3)
         
 
     cv2.imshow('Usb Cam', frame)
@@ -229,8 +232,66 @@ while True:
         for img in images:
             cv2.imwrite('./p_image/p_image.jpg', img)  # 이미지 수 많아지면, 변경 해줘야함
 
+    elif ch == ord('o'):
+        p_image_path = './p_image/p_image.jpg'
 
+        p_image = cv2.imread(p_image_path)
 
+        image = frame
+        images = list(map(lambda b : image[b[1]+1:b[3]-1, b[0]+2:b[2]-1], box))
+        images = list(map(lambda i : cv2.resize(i, (224,224)), images))
+        
+        f_image = images[0]
+
+        grayF = cv2.cvtColor(f_image, cv2.COLOR_RGB2GRAY)
+        grayF = cv2.GaussianBlur(grayF, (0,0), 1.0)
+        grayF = cv2.resize(grayF, (224,224))
+
+        grayP = cv2.cvtColor(p_image, cv2.COLOR_BGR2GRAY)
+        grayP = cv2.GaussianBlur(grayP, (0,0), 1.0)        
+        grayP = cv2.resize(grayP, (224,224))
+
+        cv2.imwrite('./f.jpg', grayF)
+        cv2.imwrite('./p.jpg', grayP)
+
+        (score, diff) = compare_ssim(grayP, grayF, full=True)
+        diff = (diff * 255).astype('uint8')
+        #cv2.imshow('DIFF', diff)
+        text = 'SCORE: {:0.2f}'.format(score)
+        print('SSIM: {}'.format(score))
+
+        # ### PIL 이요ㅕㅇ해서 캠 옆에 ssim score 보여주기
+        # font = ImageFont.truetype('NotoMono-Regular.ttf')
+        # text_w, text_h = font.getsize(text)
+        # w = 224
+        # h = 224
+        # #X_POS = w - text_w - 10
+        # #Y_POS = h - text_h - 10
+
+        # X_POS =  5
+        # Y_POS =  5
+
+        # pil_image = Image.fromarray(f_image)
+        # draw = ImageDraw.Draw(pil_image)
+        # draw.text((X_POS,Y_POS), text, (0,0,255), font=font)
+        # f_image = np.array(pil_image)
+
+        if score < 0.91:
+            #cv2.rectangle(f_image, (0,0), (w,h), (0,0,255), 6)   # 6은 두께
+            #print('There is Object on 1~3 column')
+            print('1~3 컬럼에 무엇인가가 있다!!!! 확인요망!!!!!!!!!!')
+
+            
+            # cv2.namedWindow('Before Opened', cv2.WINDOW_NORMAL)
+            # cv2.resizeWindow('Before Opened', 500,500)
+            # cv2.namedWindow('After Closed', cv2.WINDOW_NORMAL)
+            # cv2.resizeWindow('After Closed', 500,500)
+            
+            # cv2.imshow('Before Opened', p_image)
+            # cv2.imshow('After Closed', f_image)
+
+        else:
+            print('Fine')
 
 
 cap.release()
